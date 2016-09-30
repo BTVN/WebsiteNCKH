80f1ae209557ff9c67f90cd3242ee1b9d2d9469f
Nguyễn Quốc Trường <14110425@student.hcmute.edu.vn>
2016-09-30 07:55:27 +0700

    Add files via upload

---
+++ DieuHuongpage/DieuHuongTrang.js
@@ -0,0 +1,161 @@
+//SV
+function ThongBao()
+        {
+			location = "SV_ThongBao.html";
+        }
+		function ThongTinCaNhan()
+		{
+			location = "SV_ThongTinCaNhan.html";
+		}
+		function TrangThaiDeTai()
+		{
+			location = "SV_TrangThaiDeTai.html";
+		}
+		
+		function DiemDanhGia()
+		{
+			location ="SV_DiemDanhGia.html";
+		}
+		function DangKyDeTai()
+		{
+			location = "SV_DangKyDeTai.html";
+		}
+		function GiaHanDeTai()
+		{
+			location ="SV_GiaHanDeTai.html"
+		}
+		function HuyDeTai() {
+		    location = "SV_HuyDeTai.html"
+		}
+		function NopBaoCao() {
+		    location = "SV_NopBaoCao.html"
+		}
+		function XemMaSoDeTai() {
+		    location = "SV_XemMaDeTai.html"
+		}
+//
+		function DangXuat() {
+		    location = "DangNhap.html"
+		}
+//
+		function DangNhap() {
+		    location = "DangNhap.html"
+		}
+//Admin
+		function TrangchuAdmin() {
+		    location = "Admin_TrangChu.html"
+		}
+		function ThongBaoAdmin() {
+		    location = "Admin_ThongBao.html"
+		}
+		function QuanLyTaiKhoanAdmin() {
+		    location = "Admin_QuanLyTaiKhoan.html"
+		}
+		function ImpostFileAdmin() {
+		    location = "Admin_ImpostFile.html"
+		}
+//GV
+		function DeTaiCuaToiGV() {
+		    location = "GV_DeTaiCuaToi.html"
+		}
+		function DangKyDeTaiGV() {
+		    location = "GV_DangKyDeTai.html"
+		}
+		function DuyetDangKyDeTaiGV() {
+		    location = "GV_DuyetDK.html"
+		}
+		function DuyetHuyDeTaiGV() {
+		    location = "GV_DuyetHuy.html"
+		}
+		function GiaHanDeTaiGV() {
+		    location = "GV_GiaHanDeTai.html"
+		}
+		function DeTaiDangHuongDanGV() {
+		    location = "GV_HuongDan.html"
+		}
+		function HuyDeTaiGV() {
+		    location = "GV_HuyDeTai.html"
+		}
+		function NopBaoCaoGV() {
+		    location = "GV_NopBaoCao.html"
+		}
+		function DeTaiDuocPhanCongPhanBienGV() {
+		    location = "GV_PhanBien.html"
+		}
+		function DeTaiDuocPhanCongPheDuyetGV() {
+		    location = "GV_PheDuyet.html"
+		}
+		function TrangChuGV() {
+		    location = "GV_TrangChu.html"
+		}
+		function TrangThaiDeTaiGV() {
+		    location = "GV_TrangThaiDeTai.html"
+		}
+		function XemDeTaiGV() {
+		    location = "GV_XemDeTai.html"
+		}
+		function XemMaDeTaiGV() {
+		    location = "GV_XemMaDeTai.html"
+		}
+		function ThongTinGV() {
+		    location = "GV_ThongTinCaNhan.html"
+		}
+//NV
+		//GV
+		function DeTaiCuaToiNV() {
+		    location = "NV_DeTaiCuaToi.html"
+		}
+		function DangKyDeTaiNV() {
+		    location = "NV_DangKyDeTai.html"
+		}
+		function DuyetDangKyDeTaiNV() {
+		    location = "NV_DuyetDK.html"
+		}
+		function DuyetHuyDeTaiNV() {
+		    location = "NV_DuyetHuy.html"
+		}
+		function GiaHanDeTaiNV() {
+		    location = "NV_GiaHanDeTai.html"
+		}
+		function DeTaiDangHuongDanNV() {
+		    location = "NV_HuongDan.html"
+		}
+		function HuyDeTaiNV() {
+		    location = "NV_HuyDeTai.html"
+		}
+		function NopBaoCaoNV() {
+		    location = "NV_NopBaoCao.html"
+		}
+		function DeTaiDuocPhanCongPhanBienNV() {
+		    location = "NV_PhanBien.html"
+		}
+		function DeTaiDuocPhanCongPheDuyetNV() {
+		    location = "NV_PheDuyet.html"
+		}
+		function TrangChuNV() {
+		    location = "NV_TrangChu.html"
+		}
+		function TrangThaiDeTaiNV() {
+		    location = "NV_TrangThaiDeTai.html"
+		}
+		function XemDeTaiNV() {
+		    location = "NV_XemDeTai.html"
+		}
+		function XemMaDeTaiNV() {
+		    location = "NV_XemMaDeTai.html"
+		}
+		function ThongTinNV() {
+		    location = "NV_ThongTinCaNhan.html"
+		}
+		function CapMaDeTaiNV() {
+		    location = "NV_CapMaDeTai.html"
+		}
+		function PostThongBaoNV() {
+		    location = "NV_PostThongBao.html"
+		}
+		function TimKiemNV() {
+		    location = "NV_TimKiem.html"
+		}
+		function ThayDoiTrangThaiNV() {
+		    location = "NV_ThayDoiTrangThai.html"
+		}
